59f4027cbccfd665ad51b433cf18b33c6e187b0d
Vlad Dinescu <dinescu.vlad241@gmail.com>
2020-07-24 09:43:20 +0100

    SignUp page Tests - Password Match Check

--- cypress/integration/signUp.spec.js
+++ cypress/integration/signUp.spec.js
@@ -20,4 +20,12 @@ describe('Signup page', () => {
         cy.get('button[class="cta-1"]').dblclick()
         cy.get('span[class="error"]').contains('The email you have entered is not valid').should('be.visible')
     })
+
+    it('validates that the passwords entered must match', () => {
+        cy.get('input[name="email"]').type(faker.internet.email())
+        cy.get('input[name="password"]').type(Cypress.env('validPassword'))
+        cy.get('input[name="confirmPassword"]').type(Cypress.env('invalidPassword'))
+        cy.get('button[class="cta-1"]').click()
+        cy.get('span[class="error"]').contains('Passwords do not match').should('be.visible')
+    })
 })
