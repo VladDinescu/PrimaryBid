f8006beeb480c0f8aa6a1acd1487358d9c89ab56
Vlad Dinescu <dinescu.vlad241@gmail.com>
2020-07-24 09:37:52 +0100

    News Page Tests - All Articles section displays articles when Webinar Filter option is selected

--- cypress/integration/news.spec.js
+++ cypress/integration/news.spec.js
@@ -27,4 +27,17 @@ describe('News page', () => {
 
     })
 
+    it('has articles under the All Articles section when the Webinar option is selected', () => {
+        //Select the Webinar Filter
+        cy.get('[class="pb_news_filter__itemLabel_1iwa"]').contains("Webinar").click({ force: true })
+
+        //Check that the Webinar category yielded results in All Articles Section
+        cy.get('[class="card-grid margin--l"]').last()
+            .children().should('not.be.empty')
+
+        //Check if the last News article fits under the Webinar Category
+        cy.get('[class="pb_news_item__category_2vY1"]').last().should('contain', 'Webinar')
+
+    })
+
 })
