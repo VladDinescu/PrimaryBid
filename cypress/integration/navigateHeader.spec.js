d29a9f1ecfb8d09fb425759c60485f7545b422ed
Vlad Dinescu <dinescu.vlad241@gmail.com>
2020-07-24 09:32:25 +0100

    Navigate Header Tests - Visit News + Visit FAQs

--- cypress/integration/navigateHeader.spec.js
+++ cypress/integration/navigateHeader.spec.js
@@ -18,4 +18,29 @@ describe('Navigate Website using the PrimaryBid Header', () => {
         cy.url().should('include', Cypress.env('about'))
     })
 
+    it('Visits the News ', () => {
+        cy.get('[href="'+ Cypress.env('news') + '"]')
+            .contains('News')
+            .click()
+
+        cy.url().should('include', Cypress.env('news'))
+
+        cy.contains('Featured Content')
+            .should('be.visible')
+
+        cy.contains('All Content')
+            .should('be.visible')
+    })
+
+    it('Visits the FAQs', () => {
+        cy.get('[href="'+ Cypress.env('faqs') + '"]')
+            .contains('FAQs')
+            .click()
+
+            cy.url().should('include', Cypress.env('faqs'))
+
+        cy.contains('Frequently Asked Questions')
+        .should('exist')
+    })
+
 })
